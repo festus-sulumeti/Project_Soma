1105b069bdf6c77c71b9e9cf4c5bef18fb2e5f78
Johnson Mbeba <john.mbeba@student.moringaschool.com>
2024-01-21 00:15:01 +0300

    Updated files

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,7 +1,8 @@
-import { UserAddOutlined, UserOutlined } from '@ant-design/icons'
-import { Link } from 'react-router-dom'
+import React from 'react'
 import Logo from './Logo'
 import { Button } from './ui/button'
+import { MoveRight } from 'lucide-react'
+import { Link } from 'react-router-dom'
 
 const Navbar = () => {
   return (
@@ -9,14 +10,11 @@ const Navbar = () => {
       <Logo />
       <div className="flex items-center gap-[10px]">
         <Button asChild variant="ghost">
-          <Link to={"/login"}>
-            <UserOutlined className='h-4 w-4 mr-2'/>
-            Login
-        </Link>
+          <Link to={"/login"}>Login</Link>
         </Button>
         <Button asChild>
           <Link to={"/signup"}>
-            <UserAddOutlined className="h-4 w-4 mr-2" />
+            <MoveRight className="h-4 w-4 mr-2" />
             Sign up
           </Link>
         </Button>
---
+++ client/src/main.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import Navbar from './components/Navbar.jsx'
+import { BrowserRouter } from 'react-router-dom'
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <BrowserRouter>
+    <Navbar />
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+);
