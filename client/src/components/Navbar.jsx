7ccaeeb76e33fd1491d2478134d1542885166005
Johnson Mbeba <john.mbeba@student.moringaschool.com>
2024-01-21 00:20:43 +0300

    Updated navbar icons

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,8 +1,9 @@
-import React from 'react'
-import Logo from './Logo'
-import { Button } from './ui/button'
-import { MoveRight } from 'lucide-react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import Logo from "./Logo";
+import { Button } from "./ui/button";
+import { MoveRight } from "lucide-react";
+import { Link } from "react-router-dom";
+import { UserAddOutlined, UserOutlined } from "@ant-design/icons";
 
 const Navbar = () => {
   return (
@@ -10,17 +11,20 @@ const Navbar = () => {
       <Logo />
       <div className="flex items-center gap-[10px]">
         <Button asChild variant="ghost">
-          <Link to={"/login"}>Login</Link>
+          <Link to={"/login"}>
+            <UserOutlined className="mr-2 w-4 h-4"/>
+            Login
+          </Link>
         </Button>
         <Button asChild>
           <Link to={"/signup"}>
-            <MoveRight className="h-4 w-4 mr-2" />
+            <UserAddOutlined className="h-4 w-4 mr-2" />
             Sign up
           </Link>
         </Button>
       </div>
     </div>
   );
-}
+};
 
-export default Navbar
+export default Navbar;
