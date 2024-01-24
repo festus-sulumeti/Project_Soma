30a42e362c03f49d37d19a6579f5a4c2af0d1c64
Evie-Mututa <evemututa29@gmail.com>
2024-01-24 16:57:52 +0300

    School Planning Added

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -36,25 +36,36 @@ const Sidebar = () => {
               </AccordionTrigger>
               <AccordionContent className="flex flex-col gap-2">
                 <div className="ml-8 flex items-center gap-2">
-                  <Checkbox id="grade-1" />
+                  <Checkbox id="grade-1" rounded />
                   <label htmlFor="grade-1">Grade 1</label>
                 </div>
                 <div className="ml-8 flex items-center gap-2">
-                  <Checkbox id="grade-2" />
+                  <Checkbox id="grade-2" rounded />
                   <label htmlFor="grade-2">Grade 2</label>
                 </div>
-
-               
               </AccordionContent>
             </AccordionItem>
           </AccordionContent>
         </AccordionItem>
+        
+        {/* School Planning Section */}
+        <AccordionItem value="school-planning">
+          <AccordionTrigger className="text-[24px]">School Planning</AccordionTrigger>
+          <AccordionContent className="ml-6">
+            <div className="flex flex-col gap-2">
+              <Checkbox id="term-events" rounded />
+              <label htmlFor="term-events" className="ml-2">Term Events</label>
+
+              <Checkbox id="exams" rounded />
+              <label htmlFor="exams" className="ml-2">Exams</label>
+
+              <Checkbox id="library" rounded />
+              <label htmlFor="library" className="ml-2">Library</label>
+            </div>
+          </AccordionContent>
+        </AccordionItem>
+        {/* End of School Planning Section */}
       </Accordion>
-
-       {/* the School planning and events */}
-
-        {/* End of the School planning and events */}
-
     </div>
   );
 };
