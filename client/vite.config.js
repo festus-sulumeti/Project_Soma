70078ac6beb3b1014fe5a77d8913a7824d9b3695
festus-sulumeti <sulumetifestus@outlook.com>
2024-12-20 22:16:12 +0300

    project files

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,4 +1,4 @@
-import path from "path";
+import path from "node:path"; // Ensure Node.js "path" module is explicitly used
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
@@ -6,7 +6,19 @@ export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      "@": path.resolve(__dirname, "src"), // Adjusted to be consistent
+    },
+  },
+  build: {
+    sourcemap: false, // Disable sourcemaps if they're causing errors
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            return id.toString().split("node_modules/")[1].split("/")[0];
+          }
+        },
+      },
     },
   },
 });
