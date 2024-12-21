1f070e075bc877fe7bf646b62f8ed270fc45dcac
festus-sulumeti <sulumetifestus@outlook.com>
2024-12-21 11:56:49 +0300

    fix: update tailwind.config.js to use ES module syntax for compatibility

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,4 +1,6 @@
 /** @type {import('tailwindcss').Config} */
+import tailwindcssAnimate from "tailwindcss-animate";
+
 export default {
   darkMode: ["class"],
   content: [
@@ -73,5 +75,5 @@ export default {
       },
     },
   },
-  plugins: [require("tailwindcss-animate")],
+  plugins: [tailwindcssAnimate],
 };
